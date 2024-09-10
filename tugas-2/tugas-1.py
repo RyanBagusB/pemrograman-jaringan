76d86371c4633bbe90650427d4bedcf993f728c7
Ryan Bagus <ryanbagusbimantoro@gmail.com>
2024-09-10 19:03:17 +0700

    Menambahkan soal 1 ke folder tugas 2

---
+++ tugas-2/tugas-1.py
@@ -0,0 +1,6 @@
+# main.py
+from module.konversi_nilai import nilai_angka_ke_huruf
+
+nilai = int(input("Masukkan nilai angka: "))
+nilai_huruf = nilai_angka_ke_huruf(nilai)
+print(f"Nilai huruf: {nilai_huruf}")
