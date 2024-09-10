2fba6e6c550688d187b092de2eb1f1ef4542ba9f
Ryan Bagus <ryanbagusbimantoro@gmail.com>
2024-09-10 18:17:52 +0700

    Menambahkan folder tugas 1 dan soal ke 2

---
+++ tugas-1/soal-2.py
@@ -0,0 +1,6 @@
+x = int(input("Masukkan nilai x: "))
+if x != 0:
+    f_x = 2 * 3 + 2 * x + 15 / x
+    print("Nilai f(x):", f_x)
+else:
+    print("Nilai x tidak boleh 0 karena ada pembagian dengan x")
