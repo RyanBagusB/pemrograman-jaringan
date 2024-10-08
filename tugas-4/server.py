5bd35b27de4fc297b5aa3372cd977edb3ca1c285
Ryan Bagus <ryanbagusbimantoro@gmail.com>
2024-10-09 00:43:49 +0700

    Add Tugas 4

--- tugas-4/server.py
+++ tugas-4/server.py
@@ -1,7 +1,16 @@
 import socket
 import argparse
 
-host = 'localhost'  # Adjust this when using a different IP
+def get_local_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.connect(("8.8.8.8", 80))
+        ip_address = s.getsockname()[0]
+    finally:
+        s.close()
+    return ip_address
+
+host = get_local_ip()
 data_payload = 2048
 backlog = 5
 
